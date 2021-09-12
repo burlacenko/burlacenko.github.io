77f62841901044ac772c2080c91db400a04cb680
Burlacenko <87153573+burlacenko@users.noreply.github.com>
2021-09-12 08:47:20 -0300

    clean unnecessary files

--- data/data.js
+++
@@ -1,9 +0,0 @@
-// fetch request
-const boston = fetch('https://burlacenko.github.io/data/bostonData.json')
-	.then(response => response.json());
-
-export default await boston;
-
-
-// "await" keyword before specifying the constant boston to export
-// This means any other modules which include this one will wait until colors has been downloaded and parsed before using it.
