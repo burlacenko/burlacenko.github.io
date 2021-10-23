434c5c6156faba362afc58e7b52cafc772671e66
Burlacenko <87153573+burlacenko@users.noreply.github.com>
2021-10-23 18:30:59 -0300

    w13 proj BuildFormikForm (BFF) added basic fields

--- FormikForm/src/App.js
+++ FormikForm/src/App.js
@@ -1,15 +1,38 @@
 import React from "react";
 // TODO: import useFormik from formik library
+import {useFormik} from 'formik';
 
 function App() {
   // TODO: add a const called formik assigned to useFormik()
+  const formik = useFormik({
+    
+    initialValues: {
+      email: '',
+      password: ''
+    },
+
+    onSubmit: values => {
+      console.log('form:', values);
+    },
+    
+    // for validation we added this:
+    validate: values => {
+      let errors = {};
+      if (!values.email) errors.email = 'Required';
+      if (!values.password) errors.password = 'Required';
+      return errors;
+    }
+  });
 
   return (
     <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Email: <input name="email" type="text" onChange={formik.handleChange} value={formik.values.email} /></div>
+        {formik.errors.email ? <div style={{color:'red'}}>{formik.errors.email}</div>: null}
+        <div>Password: <input name="password" type="text" onChange={formik.handleChange} value={formik.values.password} /></div>
+        {formik.errors.password ? <div style={{color:'red'}}>{formik.errors.password}</div>: null}
+        <button type="submit">Submit</button>      
+      </form>
     </div>
   );
 }
