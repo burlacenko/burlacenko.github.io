437b49b12e442a4397cde646c34074b6470d631b
Burlacenko <87153573+burlacenko@users.noreply.github.com>
2021-12-22 06:55:36 -0300

    w20 video20-13 TDL App first test is ok

--- ToDoListApp/todolist/src/App.test.js
+++ ToDoListApp/todolist/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+
+// professor removed this (why? since it was working):
+// import { render, screen } from '@testing-library/react';
+
+import * as ReactDOM from 'react-dom';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// professor removed this (why? since it was working):
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('ToDo professor test1', () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<App />, root);
 });
