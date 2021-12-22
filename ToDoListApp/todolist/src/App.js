9f3188b3f836f495113f9bac2d3a3fd2bfc5c2d0
Burlacenko <87153573+burlacenko@users.noreply.github.com>
2021-12-22 06:35:35 -0300

    w20 ToDoList app is running

--- ToDoListApp/todolist/src/App.js
+++ ToDoListApp/todolist/src/App.js
@@ -1,23 +1,86 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's Learn React!
-        </a>
-      </header>
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'build todo app',
+      isCompleted: false,
+    }        
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isCompleted:false}];
+    setTodos(newTodos);
+  }
+  const removeTodo = index => {
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+
+  function Todo({todo,index,remove}){
+    function handle(){
+      console.log('Ping:',index);
+      remove(index);
+    }
+    const btnDelete = <button
+      id={"btn"+index}
+      className="btnDelete"
+      onClick={ () => {
+        remove(index);
+      } }
+    >X
+    </button>;
+  
+    return (
+     <div>
+      <div className="todo" onClick={handle}>{todo.text}{btnDelete}</div>
+      <div></div>
+     </div>
+    )
+  }
+
+  function TodoForm({addTodo}){
+    const [value,setValue] = React.useState('');
+    
+    const handleSubmit = e => {
+      e.preventDefault();
+      if(!value) return;
+      addTodo(value);
+      setValue('');
+    }
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          className="input"
+          value={value}
+          placeholder="Add Todo..."
+          onChange={e => setValue(e.target.value)} />
+      </form>
+    )
+  }
+
+  return(
+    <div className="app">
+      {/* <div className='title'>ToDo List</div> */}
+      <div className="todo-list" >
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
