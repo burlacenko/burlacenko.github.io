798089a55611e3979502da01a0fdb061fe276ba5
Burlacenko <87153573+burlacenko@users.noreply.github.com>
2022-01-07 09:25:37 -0300

    PBA app fixed added bootstrap to allow NavBar collapse

--- BankApp/bank_client/src/Components/NavBar.js
+++ BankApp/bank_client/src/Components/NavBar.js
@@ -21,7 +21,7 @@ const NavBar = () => {
         return <span className="material-icons">account_circle</span>;
       }
       return <></>;
-    }
+    };
     
     // <nav className="navbar navbar-expand-lg navbar-dark bg-light">
   
@@ -29,7 +29,7 @@ const NavBar = () => {
       <div>
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#" title="Our home page">Home
+        <a className="navbar-brand" href="#/" title="Our home page">Home
         </a>
         <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
