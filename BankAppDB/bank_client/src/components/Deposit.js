45cc577998164847f0d7c995000d5b46d7357989
Burlacenko <87153573+burlacenko@users.noreply.github.com>
2022-02-17 14:35:28 -0300

    Removed BankApp 3 tiers from this repo

--- BankAppDB/bank_client/src/components/Deposit.js
+++
@@ -1,9 +0,0 @@
-import ATM from './Atm.js';
-
-function Deposit(){
-  return (
-    <ATM isDeposit={ true }/>
-  )
-};
-
-export default Deposit;
--- BankAppDB/bank_client/src/components/Withdraw.js
+++
@@ -1,9 +0,0 @@
-import ATM from './Atm.js';
-
-function Withdraw(){
-  return (
-    <ATM isDeposit={ false }/>
-  )
-};
-
-export default Withdraw;
